838bc66924a3ff7b19178677ba53705c644ff5a5
Sam Kirkiles <samkirkiles@Sams-MacBook-Air.local>
2024-05-25 20:24:33 -0400

    Add page and dashboard

---
+++ personal-site/src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+export default function Page() {
+    return (
+    <div style={{width: "100%", overflow:"auto"}}>
+        <div style={{float:"left", width: "80%"}}>
+          <p>left</p>
+        </div>
+        <div style={{float:"right", width: "80%"}}>
+          <p>right</p>
+        </div>
+    </div>
+    )
+  }
+  
